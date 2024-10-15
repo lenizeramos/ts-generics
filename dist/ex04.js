12f8d7872fe7b3afded7387e6622c6a8eade48bb
Lenize Ramos <lenizemr@gmail.com>
2024-10-14 19:50:28 -0700

    ex04 completed

--- dist/ex04.js
+++ dist/ex04.js
@@ -2,6 +2,17 @@
 // Write a generic function called `getFirstElement` that accepts an array of type `T[]` and returns the first element.
 // Create test cases using arrays of various types to demonstrate that the function works correctly.
 // Don't forget to cover the situation where the array is empty.
-const getFirstElement = () => {
-    // Code here
+const getFirstElement = (arr) => {
+    if (arr.length === 0) {
+        return `Array is empty`;
+    }
+    return arr[0];
 };
+console.log(getFirstElement(["blue", "pink", "red", "black"]));
+console.log(getFirstElement([1, 2, 3, 4, 5]));
+console.log(getFirstElement([
+    { name: "Alice", age: 30 },
+    { name: "Bob", age: 25 },
+    { name: "Char", age: 35 },
+]));
+console.log(getFirstElement([]));
--- exercises/ex04.ts
+++ exercises/ex04.ts
@@ -2,6 +2,20 @@
 // Create test cases using arrays of various types to demonstrate that the function works correctly.
 // Don't forget to cover the situation where the array is empty.
 
-const getFirstElement = () => {
-  // Code here
+const getFirstElement = <T>(arr: T[]): T | string => {
+  if (arr.length === 0) {
+    return `Array is empty`
+  }
+  return arr[0]
 };
+
+console.log(getFirstElement(["blue", "pink", "red", "black"]));
+console.log(getFirstElement([1, 2, 3, 4, 5]));
+console.log(
+  getFirstElement([
+    { name: "Alice", age: 30 },
+    { name: "Bob", age: 25 },
+    { name: "Char", age: 35 },
+  ])
+);
+console.log(getFirstElement([]));
