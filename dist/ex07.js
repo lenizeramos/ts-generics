fa8f129382682a6bc951dfcfd50e97626a8b5b87
Lenize Ramos <lenizemr@gmail.com>
2024-10-14 22:18:20 -0700

    wip

--- dist/ex07.js
+++ dist/ex07.js
@@ -2,6 +2,13 @@
 // Implement a generic function called `filterByType` that takes an array of type `T[]` and a type `U`.
 // It should return a new array containing only elements of type `U`.
 // Test the function with arrays containing various types and different target types `U`.
-const filterByType = () => {
-    // Code here
+const filterByType = (arr, type) => {
+    let aarray = [];
+    arr.forEach((element) => {
+        if (typeof element === type) {
+            aarray.push(element);
+        }
+    });
+    return aarray;
 };
+console.log(filterByType([1, 4, "6", "hey"], "number"));
--- exercises/ex07.ts
+++ exercises/ex07.ts
@@ -2,6 +2,14 @@
 // It should return a new array containing only elements of type `U`.
 // Test the function with arrays containing various types and different target types `U`.
 
-const filterByType = () => {
-  // Code here
+const filterByType = <T, U>(arr: T[], type: U): U[] => {
+  let aarray: U[] = [];
+  arr.forEach((element) => {
+    if (typeof element === type) {
+      aarray.push(element as U);
+    }
+  });
+  return aarray;
 };
+
+console.log(filterByType([1, 4, "6", "hey"], "number"));
