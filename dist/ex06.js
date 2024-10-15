89e6c859cbc0863ac8108eebd1ba44562e1bcbc7
Lenize Ramos <lenizemr@gmail.com>
2024-10-14 20:49:58 -0700

    ex06 completed

--- dist/ex06.js
+++ dist/ex06.js
@@ -1,6 +1,9 @@
 "use strict";
 // Define a generic function named `createPair` that takes two arguments of types `A` and `B` and returns them as a pair within an array `[A, B]`.
 // Provide test cases using different types for `A` and `B` parameters.
-const createPair = () => {
-    // Code here
+const createPair = (input1, input2) => {
+    return [input1, input2];
 };
+console.log(createPair(1, 2));
+console.log(createPair("1", 2));
+console.log(createPair({ name: "Alice" }, [1, 2])); // Output: [{ name: "Alice" }, [1, 2]]
