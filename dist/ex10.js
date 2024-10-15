4db8e2965362a983e99bb702c88b1b41ec7aa2b1
Lenize Ramos <lenizemr@gmail.com>
2024-10-15 11:17:08 -0700

    ex10 completed

--- dist/ex10.js
+++ dist/ex10.js
@@ -1,3 +1,13 @@
 "use strict";
 // Develop a generic function named `customMap` that takes an array of type `T[]` and a mapping function `mapper: (item: T) => U`, and returns a new array of type `U[]`.
 // Test the function with arrays of different types and custom mapping functions.
+function customMap(array, mapper) {
+    const mapArray = [];
+    for (const item of array) {
+        mapArray.push(mapper(item));
+    }
+    return mapArray;
+}
+console.log(customMap([1, 2, 3, 4, 5], (num) => num * 2));
+console.log(customMap(["Amanda", "Bea", "Carol"], (str) => str.length));
+console.log(customMap([{ name: "Leni" }, { name: "Bianca" }], (obj) => obj.name.toUpperCase()));
--- exercises/ex10.ts
+++ exercises/ex10.ts
@@ -1,2 +1,15 @@
 // Develop a generic function named `customMap` that takes an array of type `T[]` and a mapping function `mapper: (item: T) => U`, and returns a new array of type `U[]`.
 // Test the function with arrays of different types and custom mapping functions.
+
+function customMap<T, U>(array: T[], mapper: (item: T) => U): U[] {
+  const mapArray: U[] = [];
+
+  for (const item of array) {
+    mapArray.push(mapper(item));
+  }
+  return mapArray;
+}
+
+console.log(customMap([1, 2, 3, 4, 5], (num) => num*2));
+console.log(customMap(["Amanda", "Bea", "Carol"], (str) => str.length));
+console.log(customMap([{name: "Leni"}, {name: "Bianca"}], (obj) => obj.name.toUpperCase()));
