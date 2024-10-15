4df77ffa64a6e240bfdab0f0c808f915da009d53
Lenize Ramos <lenizemr@gmail.com>
2024-10-14 17:39:02 -0700

    ex01 completed

--- exercises/ex01.ts
+++ exercises/ex01.ts
@@ -2,6 +2,16 @@
 // These arguments could be strings or numbers. If both are strings, use the concat method; otherwise, just sum the numbers.
 // If they are not of the same type, print an error message to the console; If they are not string or number, print an error message
 
-const concatSum = () => {
-  //Code here
+const concatSum = <T, S>(input1: T, input2: S): number |string => {
+  if (typeof input1 === "string" && typeof input2 === "string") {
+    return input1 + " " + input2;
+  }
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    return input1 + input2;
+  }
+  return `Error: inputs must be either both strings or both numbers.`;
 };
+
+console.log(concatSum("hey", "Leni"));
+console.log(concatSum(15, 20));
+console.log(concatSum("hey", 20));
