02b3faf98c5a08b4d190af047d4a3a3d7cffb4a4
Lenize Ramos <lenizemr@gmail.com>
2024-10-14 17:40:18 -0700

    ex02 completed

--- dist/ex02.js
+++ dist/ex02.js
@@ -1,6 +1,11 @@
 "use strict";
 // Create a generic function named `identity` that takes a single argument of type `T` and returns that argument.
 // Create a few test cases, such as using the function with numbers, strings, and custom objects to demonstrate its type flexibility.
-const identify = () => {
-    // Code here
+const identify = (data) => {
+    return data;
 };
+console.log(identify("Elisson"));
+console.log(identify(27));
+console.log(identify({ name: "Ale", age: 33 }));
+console.log(identify(["blue", "pink", 19, 25, true]));
+console.log(identify(() => { }));
