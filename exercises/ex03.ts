7c10cf647d724970126540b2b27dd2c91cf0ad76
Lenize Ramos <lenizemr@gmail.com>
2024-10-14 18:42:49 -0700

    ex03 completed

--- exercises/ex03.ts
+++ exercises/ex03.ts
@@ -1,6 +1,17 @@
 // Implement a generic function named `reverseArray` that takes an array of type `T[]` and reverses the order of elements in the same array.
 // Provide test cases using arrays of different types, including numbers, strings, and custom objects.
 
-const reverseArray = () => {
-  // Code here
+const reverseArray = <T>(arr: T[]): T[] => {
+  let reverse = arr.reverse();
+  return reverse;
 };
+
+console.log(reverseArray(["blue", "pink", "red", "black"]));
+console.log(reverseArray([1, 2, 3, 4, 5]));
+console.log(
+  reverseArray([
+    { name: "Alice", age: 30 },
+    { name: "Bob", age: 25 },
+    { name: "Char", age: 35 },
+  ])
+);
