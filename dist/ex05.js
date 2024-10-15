3171b88f72c192e426921c7252a58b4139461670
Lenize Ramos <lenizemr@gmail.com>
2024-10-14 20:15:34 -0700

    ex05 completed

--- dist/ex05.js
+++ dist/ex05.js
@@ -1,3 +1,11 @@
 "use strict";
 // Develop a generic function named `duplicateElements` that takes an array of type `T[]` and a number `n`, and returns a new array with each element duplicated `n` times.
 // Test the function with different types of arrays and values of `n`.
+const duplicateElements = (arr, n) => {
+    return arr.flatMap((element) => Array(n).fill(element));
+};
+console.log(duplicateElements([1, 2, 3], 2)); // [1, 1, 2, 2, 3, 3]
+console.log(duplicateElements(["blue", "pink"], 3)); // ["blue", "blue", "blue", "pink", "pink", "pink"]
+console.log(duplicateElements([{ name: "Alice" }, { name: "Bob" }, { name: "Char" }], 2));
+console.log(duplicateElements([true, false], 1)); // [true, false]
+console.log(duplicateElements([], 5)); // []
--- exercises/ex05.ts
+++ exercises/ex05.ts
@@ -1,2 +1,14 @@
 // Develop a generic function named `duplicateElements` that takes an array of type `T[]` and a number `n`, and returns a new array with each element duplicated `n` times.
 // Test the function with different types of arrays and values of `n`.
+
+const duplicateElements = <T>(arr: T[], n: number): T[] => {
+  return arr.flatMap((element) => Array(n).fill(element));
+};
+
+console.log(duplicateElements([1, 2, 3], 2)); // [1, 1, 2, 2, 3, 3]
+console.log(duplicateElements(["blue", "pink"], 3)); // ["blue", "blue", "blue", "pink", "pink", "pink"]
+console.log(
+  duplicateElements([{ name: "Alice" }, { name: "Bob" }, { name: "Char" }], 2)
+);
+console.log(duplicateElements([true, false], 1)); // [true, false]
+console.log(duplicateElements([], 5)); // []
